5001cef8cf62cc2dc421a6f5d5ee3f215b613f53
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2021-05-02 17:37:40 -0700

    Add files via upload

---
+++ ShadowSharkPayloadCompile.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+This script is used to compile Shadow Shark payloads.
+
+@author: Mr. Shark Spam Bot
+"""
+import distutils
+import sys
+import py2exe
+
+sys.argv.append("py2exe")
+distutils.core.setup(
+    options = {'py2exe': {'bundle_files': 1, 'excludes': ['tkinter'], 'compressed': True}},
+    windows = [{'script': "payload.py", "icon_resources": [(1, "setup1.ico")]}], 
+    zipfile = None,
+)
