8ed0eb3dabc0237721c90082ee57fe8575020f7c
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2021-06-12 14:43:27 -0700

    Update ShadowSharkPayloadCompile.py

--- ShadowSharkPayloadCompile.py
+++ ShadowSharkPayloadCompile.py
@@ -11,6 +11,6 @@ import py2exe
 sys.argv.append("py2exe")
 distutils.core.setup(
     options = {'py2exe': {'bundle_files': 1, 'excludes': ['tkinter'], 'compressed': True}},
-    windows = [{'script': "payload.py", "icon_resources": [(1, "ICON")]}], # Set icon on this line.
+    windows = [{'script': "ShadowSharkPayload.py", "icon_resources": [(1, "ICON")]}], # Set icon on this line.
     zipfile = None,
 )
