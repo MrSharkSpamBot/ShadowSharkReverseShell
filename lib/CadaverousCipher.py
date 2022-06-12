2eef597d53fb4c8525f61623ffda268baef58e6e
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2022-06-11 20:25:01 -0700

    Update CadaverousCipher.py

--- lib/CadaverousCipher.py
+++ lib/CadaverousCipher.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-A simple substitution cipher which uses randomly generated characters as substitutes.
+A polyalphabetic substitution cipher which uses randomly generated characters as substitutes.
 
 @author: Mr. Shark Spam Bot
 """
@@ -13,7 +13,7 @@ def generate_dictionary(characters):
     Parameters
     ----------
     characters : str
-        Characters to each be paired with a unique substitute.
+        Characters to each be paired with two unique substitutes.
 
     Returns
     -------
@@ -25,13 +25,16 @@ def generate_dictionary(characters):
         raise TypeError('characters must be a str object')
     dictionary = {}
     system_random = secrets.SystemRandom()
-    for character in characters:
-        substitute_character = chr(system_random.randint(0, 1000000))
-        while True:
-            if not substitute_character in dictionary.values():
-                break
+    for i in range(1, 3):
+        half_dictionary = {}
+        for character in characters:
             substitute_character = chr(system_random.randint(0, 1000000))
-        dictionary.update({character: substitute_character})
+            while True:
+                if not substitute_character in half_dictionary.values():
+                    break
+                substitute_character = chr(system_random.randint(0, 1000000))
+            half_dictionary.update({character: substitute_character})
+        dictionary.update({i: half_dictionary})
     return dictionary
 
 def encrypt(text, dictionary):
@@ -56,9 +59,14 @@ def encrypt(text, dictionary):
     if not isinstance(dictionary, dict):
         raise TypeError('dictionary must be a dict object')
     encrypted_text = ''
-    for character in text:
-        if character in dictionary.keys():
-            encrypted_text += dictionary[character]
+    dictionary1 = dictionary[1]
+    dictionary2 = dictionary[2]
+    for index, character in enumerate(text):
+        if character in dictionary1.keys() or character in dictionary2.keys():
+            if index % 2 == 0:
+                encrypted_text += dictionary2[character]
+            else:
+                encrypted_text += dictionary1[character]
         else:
             encrypted_text += character
     return encrypted_text
@@ -85,11 +93,18 @@ def decrypt(text, dictionary):
     if not isinstance(dictionary, dict):
         raise TypeError('dictionary must be a dict object')
     decrypted_text = ''
-    characters = list(dictionary.keys())
-    substitute_characters = list(dictionary.values())
-    for substitute_character in text:
-        if substitute_character in substitute_characters:
-            decrypted_text += characters[substitute_characters.index(substitute_character)]
+    dictionary1 = dictionary[1]
+    dictionary2 = dictionary[2]
+    characters1 = list(dictionary1.keys())
+    substitute_characters1 = list(dictionary1.values())
+    characters2 = list(dictionary2.keys())
+    substitute_characters2 = list(dictionary2.values())
+    for index, substitute_character in enumerate(text):
+        if substitute_character in substitute_characters1 or substitute_character in substitute_characters2:
+            if index % 2 == 0:
+                decrypted_text += characters2[substitute_characters2.index(substitute_character)]
+            else:
+                decrypted_text += characters1[substitute_characters1.index(substitute_character)]
         else:
             decrypted_text += substitute_character
     return decrypted_text
