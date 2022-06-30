1f5193d0d5a67e7a42f855995c62d27244c42856
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2022-06-30 12:17:51 -0700

    Update GenerateDictionaryKey.py

--- ConfigurationTools/GenerateDictionaryKey.py
+++ ConfigurationTools/GenerateDictionaryKey.py
@@ -1,18 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-This script generates a CadaverousCipher encryption key and writes it to dictionary_key.json.
+This configuration tool generates a CadaverousCipher encryption key and writes it to dictionary_key.json.
 
 @author: Mr. Shark Spam Bot
 """
-import string
 import json
+import string
+import argparse
 from lib import cadaverouscipher
 
-dictionary = cadaverouscipher.generate_dictionary(string.printable, 5)
-try:
-    with open('dictionary_key.json', 'w') as dictionary_key:
-        json.dump(dictionary, dictionary_key)
-    print('[+] Key successfully written to dictionary_key.json.\n')
-    print(dictionary)
-except PermissionError:
-    print('Root permissions are needed in order to write to the file dictionary_key.json.')
+def parse_args():
+    '''Get the subdictionary_count.'''
+    parser = argparse.ArgumentParser(description='''This configuration tool generates a CadaverousCipher 
+encryption key and writes it to dictionary_key.json.''')
+    parser.add_argument('-sdc', '--subdictionary-count', dest='subdictionary_count', required=True, type=int,
+                        help='The number of sub dictionaries inside of the main dictionary which each contain unique substitutes for each character.')
+    arguments = parser.parse_args()
+    subdictionary_count = arguments.subdictionary_count
+    return subdictionary_count
+
+def main(subdictionary_count):
+    '''Generate the dictionary key and safe it to dictionary_key.json.'''
+    dictionary = cadaverouscipher.generate_dictionary(string.printable, subdictionary_count)
+    try:
+        with open('dictionary_key.json', 'w') as dictionary_key:
+            json.dump(dictionary, dictionary_key)
+        print('[+] Key successfully written to dictionary_key.json.\n')
+        print(dictionary)
+    except PermissionError:
+        print('Root permissions are needed in order to write to the file dictionary_key.json.')
+
+if __name__ == '__main__':
+    main(parse_args())
