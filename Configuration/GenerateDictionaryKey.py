8d6eeb87d1a81c4002aa2ad2c421cbac9a554f2a
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2022-07-02 20:38:43 -0700

    Update GenerateDictionaryKey.py

--- Configuration/GenerateDictionaryKey.py
+++ Configuration/GenerateDictionaryKey.py
@@ -8,11 +8,12 @@ __import__('sys').path.append('..')
 import json
 import string
 import argparse
+import pyperclip
 from lib.CadaverousCipher import CadaverousCipher as cc
 
 def parse_args():
     '''Get the subdictionary_count.'''
-    parser = argparse.ArgumentParser(description='''This configuration tool generates a CadaverousCipher 
+    parser = argparse.ArgumentParser(description='''This configuration tool generates a CadaverousCipher
 encryption key and writes it to dictionary_key.json.''')
     parser.add_argument('-sdc', '--subdictionary-count', dest='subdictionary_count', required=True, type=int,
                         help='The number of sub dictionaries inside of the main dictionary which each contain unique substitutes for each character.')
@@ -26,10 +27,16 @@ def main(subdictionary_count):
     try:
         with open('dictionary_key.json', 'w') as dictionary_key:
             json.dump(dictionary, dictionary_key)
-        print('[+] Key successfully written to dictionary_key.json.\n')
-        print(dictionary)
+        print('[+] Key successfully written to dictionary_key.json.')
     except PermissionError:
         print('Root permissions are needed in order to write to the file dictionary_key.json.')
+    try:
+        pyperclip.copy(str(dictionary))
+        print('[+] Successfully copied dictionary to clipboard.')
+    except Exception as e:
+        print(e)
+        print('[-] Could not copy dictionary to clipboard.')
 
 if __name__ == '__main__':
     main(parse_args())
+    
